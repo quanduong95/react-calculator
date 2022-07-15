6d51c051faa53502a26372d176f4a20810c56017
Quan <quan.dn95@gmail.com>
2022-07-15 12:38:02 -0700

    edit some styling

--- src/Components/DigitButton.js
+++ src/Components/DigitButton.js
@@ -1,7 +1,7 @@
 import {ACTIONS} from "../actions"
 
 export default function DigitButton({ dispatch, digit }) {
-  return <button onClick={() => { dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } }) }}>
+  return <button className= "rounded" onClick={() => { dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } }) }}>
           {digit}
         </button>
 }
--- src/Components/OperationButton.js
+++ src/Components/OperationButton.js
@@ -1,7 +1,7 @@
 import {ACTIONS} from "../actions"
 
 export default function OperationButton({ dispatch, operation }) {
-  return <button onClick={() => { dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } }) }}>
+  return <button className="operation"  onClick={() => { dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } }) }}>
           {operation}
         </button>
 }
